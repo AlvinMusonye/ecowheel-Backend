a618556bea3be92445513b1b25858d5546b15c8f
AlvinMusonye <Alvinmusonye@gmail.com>
2025-06-24 19:26:34 +0300

    Update settings to load SECRET_KEY from environment variables and remove dotenv dependency

--- ecowheel/settings.py
+++ ecowheel/settings.py
@@ -18,7 +18,6 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv('.env')
 
 
 
@@ -26,7 +25,7 @@ load_dotenv('.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
