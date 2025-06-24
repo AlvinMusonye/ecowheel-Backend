e6c673bde72e173ee8bf4e440a6f321a3b369ea2
AlvinMusonye <Alvinmusonye@gmail.com>
2025-06-24 19:29:55 +0300

    Update settings to set SECRET_KEY directly in code

--- ecowheel/settings.py
+++ ecowheel/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY='django-insecure-!32a69tae8))r!t$dedb45h2ca&g7mx8)m^&zs)ajnyq2*kb^$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
